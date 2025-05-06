bb14521ec2098d4fa01de1c2ea067f8404e356ea
Mert Duyar <86599460+thejaxen@users.noreply.github.com>
2025-05-06 14:20:52 +0300

    Entity
    Entity for user is done.

---
+++ thejaxendemobank/src/main/java/com/thejaxen/thejaxendemobank/ThejaxendemobankApplication.java
@@ -0,0 +1,13 @@
+package com.thejaxen.thejaxendemobank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThejaxendemobankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThejaxendemobankApplication.class, args);
+	}
+
+}
